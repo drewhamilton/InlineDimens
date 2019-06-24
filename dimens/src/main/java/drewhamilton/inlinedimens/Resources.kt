1837b7b2a630d3eb6c3ee01e9837ce76324c9fe2
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-06-24 20:38:07 +0200

    Test Resources.kt

--- dimens/src/main/java/drewhamilton/inlinedimens/Resources.kt
+++ dimens/src/main/java/drewhamilton/inlinedimens/Resources.kt
@@ -3,8 +3,8 @@ package drewhamilton.inlinedimens
 import android.content.res.Resources
 import androidx.annotation.DimenRes
 
-fun Resources.getDimensionPxSize(@DimenRes id: Int) = PxInt(getDimensionPixelSize(id))
+fun Resources.getDimensionPx(@DimenRes id: Int) = Px(getDimension(id))
 
 fun Resources.getDimensionPxOffset(@DimenRes id: Int) = PxInt(getDimensionPixelOffset(id))
 
-fun Resources.getDimensionPx(@DimenRes id: Int) = Px(getDimension(id))
+fun Resources.getDimensionPxSize(@DimenRes id: Int) = PxInt(getDimensionPixelSize(id))
---
+++ dimens/src/test/java/drewhamilton/inlinedimens/ResourcesTest.kt
@@ -0,0 +1,31 @@
+package drewhamilton.inlinedimens
+
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.Test
+
+class ResourcesTest {
+
+    private val testId = 432
+    private val testPixelSize = 934
+    private val testPixelOffset = 2348
+    private val testDimension = 234.5345f
+
+    @Test fun `getDimensionPx gets from getDimension`() {
+        whenever(TestValues.mockResources.getDimension(testId)).thenReturn(testDimension)
+        assertThat(TestValues.mockResources.getDimensionPx(testId))
+            .isEqualTo(Px(testDimension))
+    }
+
+    @Test fun `getDimensionPxOffset gets from getDimensionPixelOffset`() {
+        whenever(TestValues.mockResources.getDimensionPixelOffset(testId)).thenReturn(testPixelOffset)
+        assertThat(TestValues.mockResources.getDimensionPxOffset(testId))
+            .isEqualTo(PxInt(testPixelOffset))
+    }
+
+    @Test fun `getDimensionPxSize gets from getDimensionPixelSize`() {
+        whenever(TestValues.mockResources.getDimensionPixelSize(testId)).thenReturn(testPixelSize)
+        assertThat(TestValues.mockResources.getDimensionPxSize(testId))
+            .isEqualTo(PxInt(testPixelSize))
+    }
+}
