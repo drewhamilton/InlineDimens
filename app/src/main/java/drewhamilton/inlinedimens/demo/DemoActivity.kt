04820bfffdf04c29237ddefe614314cd2de10f5c
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-08-03 15:47:13 +0200

    Start setting up demo app

---
+++ app/src/main/java/drewhamilton/inlinedimens/demo/DemoActivity.kt
@@ -0,0 +1,12 @@
+package drewhamilton.inlinedimens.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.demo)
+    }
+}
