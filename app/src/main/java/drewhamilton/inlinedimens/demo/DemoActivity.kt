ad6ce7ef5a8657c0188b2817f3595ed2e6f29296
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-08-04 00:54:22 +0200

    Implement demo app

--- app/src/main/java/drewhamilton/inlinedimens/demo/DemoActivity.kt
+++ app/src/main/java/drewhamilton/inlinedimens/demo/DemoActivity.kt
@@ -1,12 +1,50 @@
 package drewhamilton.inlinedimens.demo
 
+import android.graphics.Point
 import android.os.Bundle
+import android.view.Display
 import androidx.appcompat.app.AppCompatActivity
+import drewhamilton.inlinedimens.PxInt
+import drewhamilton.inlinedimens.getTextSizePx
+import drewhamilton.inlinedimens.toDp
+import drewhamilton.inlinedimens.toDpInt
+import drewhamilton.inlinedimens.toPxInt
+import drewhamilton.inlinedimens.toSp
+import drewhamilton.inlinedimens.toSpInt
+import kotlinx.android.synthetic.main.demo.screenHeightView
+import kotlinx.android.synthetic.main.demo.screenWidthView
+import kotlinx.android.synthetic.main.demo.textSizeView
 
 class DemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.demo)
+
+        val screenSize = windowManager.defaultDisplay.getRealSize()
+
+        val screenWidth = PxInt(screenSize.x)
+        screenWidthView.text = getString(
+            R.string.screenWidth,
+            screenWidth.value, screenWidth.toDp(this).toDpInt().value
+        )
+
+        val screenHeight = PxInt(screenSize.y)
+        screenHeightView.text = getString(
+            R.string.screenHeight,
+            screenHeight.value, screenHeight.toDp(this).toDpInt().value
+        )
+
+        val textSize = textSizeView.getTextSizePx()
+        textSizeView.text = getString(
+            R.string.textSize,
+            textSize.toPxInt().value, textSize.toDp(this).toDpInt().value, textSize.toSp(this).toSpInt().value
+        )
+    }
+
+    private fun Display.getRealSize(): Point {
+        val size = Point()
+        getRealSize(size)
+        return size
     }
 }
